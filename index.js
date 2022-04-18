b70484f188b42b6d3ce468758cd37d7fba99164f
Yoon <Tae.Yoon@rgi-corp.com>
2022-04-18 19:00:03 -0400

    Added new simple server branch for easy testing

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3001
-
-const pgAction = require('./components/pgAction')
+const response  = {
+	"type": "FeatureCollection",
+	"features": [{
+		"type": "feature",
+		"geometry": {
+			"type": "point",
+			"coordinates": [44.111, 34.111]
+		}
+	}]
+}
 
 app.use(express.json())
 app.use(function (req, res, next) {
@@ -11,45 +19,11 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
 });
-//const selectEverything = 'SELECT * FROM public.dsrc_record ORDER BY dsrc_id ASC, record_id ASC'
-const returnCount = 'SELECT count(*) FROM public.dsrc_record'
-
-const selectEverything  =`SELECT * FROM public.dsrc_record`
-//const selectWhere = `WHERE LOWER(json_data::json ->> 'NAME_FIRST') = LOWER('Honda') AND `
-const selectOrderBy = `ORDER BY dsrc_id ASC, record_id ASC`
-
-function createWhere(query) {
-  let whereClause =''
-  try{
-  if(query.where){
-   
-    const whereObj = JSON.parse(query.where)
-    whereClause = whereObj.map( clause => {
-     const orJoins = Object.keys(clause).map(key => {
-        return `LOWER(json_data::json ->> '${key}') like LOWER('${clause[key]}%')`
-      }).join(' OR '); 
-      return `(${orJoins})`
-    }).join('AND')
-    //console.log ('where=', whereClause)
-    // whereClause = Object.keys(whereObj).map(key => {
-    //   return `LOWER(json_data::json ->> '${key}') = LOWER('${whereObj[key]}')`
-    // }).join(' AND '); 
-   }
-  }catch (e){
-    console.log("Json text parse error")
-  }finally{
-    return whereClause ? `WHERE ${whereClause}` : whereClause
-  }
-}
 
 app.get('/', (req, res) => {
-  pgAction.listDsrcRecord(returnCount)
-  .then(response => {
+
     res.status(200).send(response);
-  })
-  .catch(error => {
-    res.status(500).send(error);
-  })
+ 
 })
 
 app.get('/all', (req, res) => { 
@@ -58,14 +32,7 @@ app.get('/all', (req, res) => {
     let orderBy = req.query.orderby ? selectOrderBy : '';
     //console.log(createWhere(req.query))
     
-    const selectWithLimit = `${selectEverything} ${createWhere(req.query)} ${orderBy} LIMIT ${limit} OFFSET ${offset}`
-    pgAction.listDsrcRecord(selectWithLimit)
-    .then(response => {
       res.status(200).send(response);
-    })
-    .catch(error => {
-      res.status(500).send(error);
-    })
   })
 
 app.listen(port, () => {
